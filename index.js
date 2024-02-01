9c5d0021ef10488122d48cb3bead93c86fcfd47d
Ethan Harris <158079676+ethanharris957@users.noreply.github.com>
2024-02-01 21:34:06 +0000

    added more JS and added to the info tables.
    Added the JavaScript for the second info table on the right hand side; regarding User's Holiday.
    i'm still new to Github so might be doing this wrong.

--- index.js
+++ index.js
@@ -19,3 +19,21 @@ elAge.textContent = age;
 
 let elPattern = document.getElementById('pattern');
 elPattern.textContent = pattern;
+
+//Declare variabes for the timeAway table
+let holidayEntitlement = 30;
+let bookedHoliday = 10;
+let remainingHoliday = 20;
+
+//Retrieve and Update empty table cells with relevant data:
+//call the 'getElementById' Method, of the 'Document' Object. Store it in a new variable.
+//Then, call the 'textContent' Method of the new declared variable to replace the content with
+//the value of the desired variable.
+let elEntitlement = document.getElementById('holidayEntitlement');
+elEntitlement.textContent = holidayEntitlement;
+
+let elBooked = document.getElementById('bookedHoliday');
+elBooked.textContent = bookedHoliday;
+
+let elRemaining = document.getElementById('remainingHoliday');
+elRemaining.textContent = remainingHoliday;
